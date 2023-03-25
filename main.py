300327e484f31fbcb5af0a25a88f4d504cd7e430
Willy <jemand771@gmx.net>
2023-03-25 17:58:36 +0100

    proxy direct url requests to allow early cache expiry

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
+import re
 
 import redis
+import requests.models
 from flask import Flask, redirect, render_template, request
 
 import worker
 from worker import ResponseMode
+
+MODE_PARAM_KEY = "ew-mode"
 
 APP = Flask(__name__)
 
@@ -26,7 +30,9 @@ def favicon():
 def all_requests(path):
     host = request.host.split(":")[0]
     if host in BASE_HOSTS:
-        return handle_url(path)
+        # discord turns bla/https://blub into bla/https:/blub
+        fixed_url = re.sub(r"^(https?:/)([^/])", r"\1/\2", path)
+        return handle_url(fixed_url)
     for base in BASE_HOSTS:
         host_parts = host.split(".")
         match_parts = base.split(".")
@@ -38,24 +44,29 @@ def all_requests(path):
                 # rewrite tiktok.embed.works to tiktok.com.embed.works in the reverse proxy to support shorter syntax
                 full_domain = ".".join(host_parts)
                 return handle_url(f"https://{full_domain}/{path}")
-
     return "unable to parse url", 400
 
 
 def handle_url(url: str):
     mode = ResponseMode.embed
     try:
-        mode = ResponseMode(request.args["ew-mode"])
+        mode = ResponseMode(request.args[MODE_PARAM_KEY])
     except (KeyError, ValueError):
         pass
     full_url = url + (f"?{request.query_string.decode()}" if request.query_string else "")
     if mode == ResponseMode.original:
-        return redirect(full_url, code=307)
+        return redirect(full_url, code=302)
     info = WK.get_info(full_url)
     # TODO auto redirect check (user agent)
     if mode == ResponseMode.direct:
-        return redirect(info.video_url, code=307)
-    return render_template("embed.html", info=info)
+        return redirect(info.video_url, code=302)
+    return render_template("embed.html", info=info, video_url=get_direct_url(request.url))
+
+
+def get_direct_url(url):
+    req = requests.models.PreparedRequest()
+    req.prepare_url(url, {MODE_PARAM_KEY: ResponseMode.direct.value})
+    return req.url
 
 
 # TODO better errors
