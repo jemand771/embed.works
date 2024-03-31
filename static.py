b67acfc8e0a9872cffc22d4a38140751941751e0
Willy <jemand771@gmx.net>
2024-03-31 23:21:25 +0200

    make bot check ignore case

--- static.py
+++ static.py
@@ -14,7 +14,8 @@ EXTRACTOR_COLOR = dict(
 # https://github.com/FixTweet/FixTweet/blob/bc7e680a0b41fd762c1ba9028090a007ce97a41e/src/constants.ts#L17-L18
 BOT_UA_REGEX = re.compile(
     r"bot|facebook|embed|got|firefox/92|firefox/38|curl|wget|go-http|yahoo|generator|whatsapp|preview|link|proxy"
-    "|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node"
+    "|vkshare|images|analyzer|index|crawl|spider|python|cfnetwork|node",
+    re.IGNORECASE
 )
 
 
